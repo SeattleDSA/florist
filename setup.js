8d96e57ff7449eb4b2fae103ec0f33989e0f9b2b
Stuart P. Bentley <stuart@testtrack4.com>
2018-04-17 12:06:27 +0000

    Register service worker after the page has loaded
    Per best practice at https://developers.google.com/web/fundamentals/primers/service-workers/registration

--- setup.js
+++ setup.js
@@ -3,12 +3,7 @@
 // service worker / infrastructure setup stuff
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-  .then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js');
   });
 }
